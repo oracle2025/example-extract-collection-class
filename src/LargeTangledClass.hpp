03dc755396669dab4d2ed694d29a6fa20eb32abd
Richard Spindler <richard.spindler@gmail.com>
2022-09-06 21:24:02 +0200

    inherits from collection class

--- src/LargeTangledClass.hpp
+++ src/LargeTangledClass.hpp
@@ -3,16 +3,19 @@
 #include <map>
 #include <string>
 #include "DataBase.hpp"
+#include "CollectionClass.hpp"
 
 struct record {
     std::string name;
     std::string value;
 };
 
-class LargeTangledClass {
+class LargeTangledClass : public CollectionClass {
 public:
     void addRecord(record r);
+
     void saveToDatabase();
+
 private:
     std::map<std::string, std::string> m_data;
     DataBase m_dataBase;
